406817203f70e11373a79e8dfd6763b39adc0e44
ptusb <ptusb@github.com>
2025-11-28 23:09:37 +0530

    Added REAL Firebase keys - Going Online!

--- firebase-config.js
+++ firebase-config.js
@@ -3,13 +3,14 @@
 // ===========================
 
 export const firebaseConfig = {
-    apiKey: "YOUR_API_KEY_HERE",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    databaseURL: "https://YOUR_PROJECT_ID-default-rtdb.firebaseio.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID"
+    apiKey: "AIzaSyBrzXIXqptcPcXQE6DW1V-ZWjX7Cty_s14",
+    authDomain: "bmcc-admission-form.firebaseapp.com",
+    databaseURL: "https://bmcc-admission-form-default-rtdb.firebaseio.com",
+    projectId: "bmcc-admission-form",
+    storageBucket: "bmcc-admission-form.firebasestorage.app",
+    messagingSenderId: "173516804832",
+    appId: "1:173516804832:web:1555d1d236fda8bf343685",
+    measurementId: "G-PREVXYPPEL"
 };
 
 // Helper to check if Firebase is configured
